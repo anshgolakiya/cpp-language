556583e8c8c64f35a9fdd74f43334afac90a30ff
anshgolakiya <anshgolakiya9909@gmail.com>
2025-02-20 21:09:09 +0530

    Delete basic c++ directory

--- basic c++/basiccode.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std ;
-int main()
-{
-    cout << "hello"<<endl<<"world c++" ; // or use /n 
-    return 0 ;
-    // code run by open folder cd , file g++ , last a.exe .
-}
